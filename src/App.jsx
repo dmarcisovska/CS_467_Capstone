a3b117372c53a4b3e927bf442a9a417b8dd80d4d
Denisa Marcisovska <dmarcisovska@gmail.com>
2025-10-11 13:32:58 -0700

    installing react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import './App.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
