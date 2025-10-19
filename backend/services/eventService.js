08a1ab384a230dd951cf9d3b471ff57828c86e34
Brian <canb@oregonstate.edu>
2025-10-19 14:23:43 -0400

    saving backend files before switching to main branch

--- backend/services/eventService.js
+++
@@ -1,6 +0,0 @@
-import { findEvents } from "../repositories/eventRepository.js"
-
-export const getEventsService = async (filters) => {
-    const events = await findEvents(filters)
-    return events;
-}
