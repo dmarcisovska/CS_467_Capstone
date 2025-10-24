b22a23c9b28a115b07c97218e678799d5b1df061
Denisa Marcisovska <dmarcisovska@gmail.com>
2025-10-23 18:38:40 -0700

    add event date, and event time, removed parking info

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -17,53 +17,58 @@ import titleImg from '../assets/trail3.jpg';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import RunCircleIcon from '@mui/icons-material/RunCircle';
-
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 const Create = () => {
   return (
     <>
-     <Box
-      sx={{
-        position: 'relative',
-        height: '400px', 
-        backgroundImage: `url(${titleImg})`, 
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: 'white',
-        textAlign: 'center',
-      }}
-    >
       <Box
-        sx={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-          zIndex: 1,
-        }}
-      />
-
-      {/* Title text */}
-      <Typography
-        variant="h2"
-        component="h1"
         sx={{
           position: 'relative',
-          zIndex: 2,
+          height: '400px',
+          backgroundImage: `url(${titleImg})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          textAlign: 'center',
         }}
       >
-        Start a Race
-      </Typography>
-    </Box>
-      <Box
-      >
-       
-        <Container sx={{ pt: 4, zIndex: 1, position: 'relative',  maxWidth: { xs: '100%', sm: '600px', md: '700px' },  }}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            zIndex: 1,
+          }}
+        />
+
+        <Typography
+          variant="h2"
+          component="h1"
+          sx={{
+            position: 'relative',
+            zIndex: 2,
+          }}
+        >
+          Start a Race
+        </Typography>
+      </Box>
+      <Box>
+        <Container
+          sx={{
+            pt: 4,
+            zIndex: 1,
+            position: 'relative',
+            maxWidth: { xs: '100%', sm: '600px', md: '700px' },
+          }}
+        >
           <Stack spacing={2}>
             <Typography
               variant="h4"
@@ -72,66 +77,83 @@ const Create = () => {
             >
               Event Details
             </Typography>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-            >
+            <Typography variant="body2" color="text.secondary">
               Enter the basics for your race.
             </Typography>
 
             <Divider />
-            <Box component="form" noValidate>
-              <Grid container spacing={2}>
-                <TextField
-                  fullWidth
-                  label="Event Name"
-                  variant="filled"
-                  sx={{
-                    mb: 2,
-                    backgroundColor: 'rgba(255, 255, 255, 0.85)',
-                    borderRadius: 1,
-                  }}
-                />
-                <TextField
-                  label="Event Description"
-                  variant="filled"
-                  fullWidth
-                  multiline
-                  rows={4}
-                
-                />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Box component="form" noValidate>
+                <Grid container spacing={2}>
+                  <TextField
+                    fullWidth
+                    label="Event Name"
+                    variant="filled"
+                    sx={{
+                      mb: 2,
+                      backgroundColor: 'rgba(255, 255, 255, 0.85)',
+                      borderRadius: 1,
+                    }}
+                  />
+                  <DatePicker
+                    label="Event Date"
+                    slotProps={{
+                      textField: {
+                        variant: 'filled',
+                        fullWidth: true,
+                        sx: {
+                          mb: 2,
+                          backgroundColor: 'rgba(255, 255, 255, 0.85)',
+                          borderRadius: 1,
+                        },
+                      },
+                    }}
+                  />
+                  <TimePicker
+                    label="Start Time"
+                    slotProps={{
+                      textField: {
+                        variant: 'filled',
+                        fullWidth: true,
+                        sx: {
+                          mb: 2,
+                          backgroundColor: 'rgba(255, 255, 255, 0.85)',
+                          borderRadius: 1,
+                        },
+                      },
+                    }}
+                  />
 
-                <TextField
-                  fullWidth
-                  label="Location (parking / map notes)"
-                  variant="filled"
-                 
-                />
+                  <TextField
+                    label="Event Description"
+                    variant="filled"
+                    fullWidth
+                    multiline
+                    rows={4}
+                  />
 
-                <TextField
-                  fullWidth
-                  label="Distance"
-                  type="number"
-                  variant="filled"
-                 
-                />
-                <TextField
-                  select
-                  fullWidth
-                  label="Units "
-                  variant="filled"
-                  
-                ></TextField>
-                <TextField
-                  fullWidth
-                  label="Elevation Gain (ft or m)"
-                  variant="filled"
-                  type="number"
-                  inputProps={{ min: 0, step: 'any' }}
-                
-                />
-              </Grid>
-            </Box>
+                  <TextField
+                    fullWidth
+                    label="Distance"
+                    type="number"
+                    variant="filled"
+                  />
+                  <TextField
+                    select
+                    fullWidth
+                    label="Units "
+                    variant="filled"
+                  ></TextField>
+                  <TextField
+                    fullWidth
+                    label="Elevation Gain (ft or m)"
+                    variant="filled"
+                    type="number"
+                    inputProps={{ min: 0, step: 'any' }}
+                  />
+                </Grid>
+              </Box>
+            </LocalizationProvider>
           </Stack>
           <Button
             variant="contained"
@@ -144,7 +166,7 @@ const Create = () => {
           </Button>
         </Container>
       </Box>
-      </>
+    </>
   );
 };
 
