749ca232bfe3493510473491898fb22e2fc30bac
Denisa Marcisovska <dmarcisovska@gmail.com>
2025-10-24 10:53:32 -0700

    new event page image

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import titleImg from '../assets/running_feet.jpg'
+import titleImg from '../assets/gigi-bIpKSEsaN6Q-unsplash.jpg';
 
 const Events = () => {
   return (
